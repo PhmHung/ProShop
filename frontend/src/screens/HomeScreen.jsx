7ae6d6115a31794cd89d9265088a4b7144198ce9
PhmHung <hungk77777@gmail.com>
2025-08-14 00:12:05 +0700

    Product info

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import products from '../products'
+import Product from '../components/Product'
 import { Col, Row } from 'react-bootstrap'
 
 const HomeScreen = () => {
@@ -7,14 +8,13 @@ const HomeScreen = () => {
     <>
         <h1>Latest Products</h1>
         <Row>
-            {products.map(product =>(
-                <Col sm={12} md={6} lg={4} xl={3}>
-                <h3>{product.name}</h3>
+            {products.map((product) =>(
+                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                 <Product product={product} />
                 </Col>
             ))}
         </Row>
     </>
   )
 }
-
 export default HomeScreen
